71a7d2d0cfece3502ff50996d6c654c0f115f264
Alec Cunningham <aleccunningham96@gmail.com>
2016-11-14 19:50:24 -0600

    mocking tests slowly

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,7 @@
 from pygci import GCivicInfo, GCivicInfoError, GCivicAuthError
 
 from .config import (
-    unittest
+    unittest, api_key
 )
 
 import responses
@@ -21,15 +21,15 @@ class GCivicInfoAPITestCase(unittest.TestCase):
         """Convenience function for mapping from endpoint to URL"""
         return '%s/%s?key=%s' % (self.api.api_url % self.api.api_version, endpoint, api_key)
 
-    def register_response(self, url, body='{}', status=200,
+    def register_response(self, url, method='GET', status=200,
                           content_type='application/json; charset=utf-8'):
         """Wrapper function for responses for simpler unit tests"""
-        responses.add(url, body, status, content_type)
+        responses.add(url, status, content_type)
 
     @responses.activate
     def test_requests_should_handle_full_endpoint(self):
         """Test that requests() accepts a full URL plus endpoint"""
-        url = 'https://wwww.googleapis.com/civicinfo/v2/elections'
+        url = 'https://wwww.googleapis.com/civicinfo/v2/elections
         self.register_response(responses.GET, url)
 
         self.requests.get(url)
